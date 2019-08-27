fb2e188a2454fe7f0acad43a4dac4ec43a2890a6
surilokesh <48464991+surilokesh@users.noreply.github.com>
2019-08-27 19:14:18 -0400

    Add files via upload

---
+++ SpringCloudConfigServiceApplication.java
@@ -0,0 +1,15 @@
+package com.practice.microservices.springcloudconfigservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@EnableConfigServer
+@SpringBootApplication
+public class SpringCloudConfigServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCloudConfigServiceApplication.class, args);
+	}
+
+}
